dd765f21f832e1377593a804ac7290ea2bcaf253
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-14 01:22:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,24 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content === 'what is my avatar') {
+        return message.channel.send(message.author.avatarURL);
+    }
+});
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("Hello!");
+    }
+})
+
 client.login(process.env.BOT_TOKEN);
