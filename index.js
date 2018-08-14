32961233cfe1c81ae182a5bd569951cc0d9e8d55
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-14 12:25:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,4 +19,16 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content === '!hello') {
+        return message.channel.send("Hello!");
+    }
+});
+
+client.on('message', message => {
+    if (message.content === 'aquech') {
+        return message.channel.send("<@335796011194777611> mongolo vete al maincra");
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
