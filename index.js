ef95a2e56ba89dcbd38838ac426848a0ee9e2d64
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-14 20:54:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,16 @@ client.on("ready", async () => {
 //!github
 client.on("message", (message) => {
     if(message.content == "!github"){
-             git(message);
+        const embed = new Discord.RichEmbed()
+        .setColor('f4eb42')
+        .setThumbnail('https://media.discordapp.net/attachments/478509969684299786/478509983659720704/AR01..jpg?width=480&height=480')
+        .setTitle("Aid3n/bot-discord-in-js")
+        .setURL("https://github.com/Aid3n/bot-discord-in-js")
+        .setDescription("You cand find my github project here!")
+        .setAuthor('Aiden', 'https://media.discordapp.net/attachments/478509969684299786/478999662053949451/38618692.jpg', 'https://github.com/Aid3n')
+        message.channel.sendEmbed(embed);
         }
-});
+})
 //!ping
 client.on("message", (message) => {
 	if(message.content == "!ping"){
