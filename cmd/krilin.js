ab95fb0fda131a3ecbbc36c536452ab5c1251306
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-14 18:41:26 +0200

    Add files via upload

---
+++ cmd/krilin.js
@@ -0,0 +1,7 @@
+module.exports = (message) => {
+    message.channel.send('Hay mi niño <3' {
+            files: [
+                "./files/krilin.jpg"
+            ]
+        }).catch(e => console.log(e));
+}
